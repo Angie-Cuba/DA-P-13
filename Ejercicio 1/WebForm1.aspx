c56bc4f046f7978167139ee15681a294affd4105
Rudy Haytara Aguirre <70269426@ucsm.edu.pe>
2021-12-03 10:49:43 -0500

    Ejercicios Propuestos  1 y 2

---
+++ Ejercicio 1/WebForm1.aspx
@@ -0,0 +1,48 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Practica_2_EjerProp.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Aplicacion web</title>
+    <style type="text/css">
+        .Imagen {
+            height: 211px;
+            width: 332px;
+            margin-left: 166px;
+        }
+
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" Text="FACULTADES" 
+            Width="479px" Height="36px" Font-Bold="True" Font-Name="Arial Black"
+            Font-Size="X-Large" ></asp:Label>
+        </div>
+        <p class="Facultades">
+            <asp:Label ID="Label2" runat="server" Text="Facultades"></asp:Label>&nbsp;
+            <asp:DropDownList ID="Productos" runat="server" Autopostback="True" OnSelectedIndexChanged="SelectedIndexChanged"></asp:DropDownList>
+        </p>
+        <p class="f1">
+            <asp:TextBox ID="f1" runat="server" Height="24px" Width="230px"></asp:TextBox>&nbsp;       
+        </p>
+        <p class="f12">
+            <asp:TextBox ID="TextBox1" runat="server" Height="24px" Width="230px"></asp:TextBox>&nbsp;       
+        </p>
+        <p class="f3">
+            <asp:TextBox ID="TextBox2" runat="server" Height="24px" Width="230px"></asp:TextBox>&nbsp;       
+        </p>
+        <p class="f4">
+            <asp:TextBox ID="TextBox3" runat="server" Height="24px" Width="230px"></asp:TextBox>&nbsp;       
+        </p>
+        <p class="f5">
+            <asp:TextBox ID="TextBox4" runat="server" Height="24px" Width="230px"></asp:TextBox>&nbsp;       
+        </p>
+        
+        
+    </form>
+</body>
+</html>
---
+++ Ejercicio 2/WebForm1.aspx.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Practica_2_EjerProp
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                InitProductos();
+            }
+        }
+        
+        protected void InitProductos()
+        {
+            Productos.Items.Add("Seleccionar");
+            Productos.Items.Add("CIENCIAS DE LA SALUD");
+            Productos.Items.Add("CIENCIAS JURIDICAS Y EMPRESARIALES");
+            Productos.Items.Add("CIENCIAS E INGENIERIAS");
+            Productos.Items.Add("CIENCIAS SOCIALES");
+
+        }
+
+        protected void SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (Productos.Items[0].Selected == true)
+            {
+                TextBox1.Enabled = false;
+                TextBox1.Text = "";
+                TextBox2.Enabled = false;
+                TextBox2.Text = "";
+                TextBox3.Enabled = false;
+                TextBox3.Text = "";
+                TextBox4.Enabled = false;
+                TextBox4.Text = "";
+                
+            }
+            if (Productos.Items[1].Selected == true)
+            {
+                TextBox1.Enabled = false;
+                TextBox1.Text = "23021";
+                TextBox2.Enabled = false;
+                TextBox2.Text = "Televisor";
+                TextBox3.Enabled = false;
+                TextBox3.Text = "Smart tv 60' OLED";
+                TextBox4.Enabled = false;
+                TextBox4.Text = "LG";
+                //Image1.Height = 350;
+                //Image1.Width = 530;
+                //Image1.ImageUrl = "~/Resources/des_1_n.jpg";
+            }
+            if (Productos.Items[2].Selected == true)
+            {
+                f1.Enabled = false;
+                f1.Text = "Administracion de empresas";
+                TextBox1.Enabled = false;
+                TextBox1.Text = "ciencia politica y gobierno";
+                TextBox2.Enabled = false;
+                TextBox2.Text = "Contabilidad";
+                TextBox3.Enabled = false;
+                TextBox3.Text = "Derecho";
+                TextBox4.Enabled = false;
+                TextBox4.Text = "ingenieria Comercial";
+                //Image1.Height = 300;
+                //Image1.Width = 300;
+                //Image1.ImageUrl = "~/Resources/16754100_1.jpg";
+            }
+            if (Productos.Items[3].Selected == true)
+            {
+                TextBox1.Enabled = false;
+                TextBox1.Text = "23023";
+                TextBox2.Enabled = false;
+                TextBox2.Text = "Microondas";
+                TextBox3.Enabled = false;
+                TextBox3.Text = "Microondas con dorador EasyClean";
+                TextBox4.Enabled = false;
+                TextBox4.Text = "LG";
+                //Image1.Height = 350;
+                //Image1.Width = 370;
+                //Image1.ImageUrl = "~/Resources/15768826_1.jpg";
+            }
+            if (Productos.Items[4].Selected == true)
+            {
+                TextBox1.Enabled = false;
+                TextBox1.Text = "23024";
+                TextBox2.Enabled = false;
+                TextBox2.Text = "Licuadora";
+                TextBox3.Enabled = false;
+                TextBox3.Text = "Licuadora 550W 2 velocidades";
+                TextBox4.Enabled = false;
+                TextBox4.Text = "Oster";
+                //Image1.Height = 300;
+                //Image1.Width = 300;
+                //Image1.ImageUrl = "~/Resources/ac0cdf781827ac14cae9cdb4b80f97f8-product.jpg";
+            }
+            /*
+            if (Productos.Items[5].Selected == true)
+            {
+                Codigo.Enabled = false;
+                Codigo.Text = "23025";
+                Nombre.Enabled = false;
+                Nombre.Text = "Tostadora";
+                Descripcion.Enabled = false;
+                Descripcion.Text = "Tostadora 750W Holstein";
+                Marca.Enabled = false;
+                Marca.Text = "Holstein";
+                Image1.Height = 300;
+                Image1.Width = 300;
+                //Image1.ImageUrl = "~/Resources/tostadora-holstein-2-rebanadas-roja-1.jpg";
+            }
+            if (Productos.Items[6].Selected == true)
+            {
+                Codigo.Enabled = false;
+                Codigo.Text = "23026";
+                Nombre.Enabled = false;
+                Nombre.Text = "Cafetera";
+                Descripcion.Enabled = false;
+                Descripcion.Text = "Cafetera Oster PrimaLatte";
+                Marca.Enabled = false;
+                Marca.Text = "Oster";
+                Image1.Height = 300;
+                Image1.Width = 370;
+                //Image1.ImageUrl = "~/Resources/2019148956993_2.jpg";
+            }
+            if (Productos.Items[7].Selected == true)
+            {
+                Codigo.Enabled = false;
+                Codigo.Text = "23027";
+                Nombre.Enabled = false;
+                Nombre.Text = "Lavadora";
+                Descripcion.Enabled = false;
+                Descripcion.Text = "Lavadora LG Miniwash con secadora";
+                Marca.Enabled = false;
+                Marca.Text = "LG";
+                Image1.Height = 350;
+                Image1.Width = 300;
+                //Image1.ImageUrl = "~/Resources/14933874_1.jpg";
+            }*/
+        }
+        
+    }
+}
